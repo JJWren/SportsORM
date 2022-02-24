e677baf2c2bfb535f8bbc8bde7fbd81cbec6ad1c
Joshua Wren <43586816+JJWren@users.noreply.github.com>
2022-02-24 00:39:13 -0600

    Updated HomeController & Related Views (Level2/3)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsORM.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace SportsORM.Controllers
@@ -69,10 +70,12 @@ namespace SportsORM.Controllers
 
             ViewBag.OrderedByLocationTeams = _context.Teams
                 .OrderBy(t => t.Location)
+                .ThenBy(t => t.TeamName)
                 .ToList();
 
             ViewBag.ReverseOrderedByTeamNameTeams = _context.Teams
                 .OrderByDescending(t => t.TeamName)
+                .ThenByDescending(t => t.Location)
                 .ToList();
 
             ViewBag.LastNameCooperPlayers = _context.Players
@@ -102,14 +105,147 @@ namespace SportsORM.Controllers
         [HttpGet("level_2")]
         public IActionResult Level2()
         {
+            ViewBag.AtlanticSoccerConf = _context.Teams
+                .Where(team => team.CurrLeague.Name.Contains("Atlantic") && team.CurrLeague.Sport.Contains("Soccer"))
+                .ToList();
+
+            ViewBag.BostonPenguinsCurrentRoster = _context.Players
+                .Where(p => p.CurrentTeam.TeamName.Contains("Penguins") && p.CurrentTeam.Location.Contains("Boston"))
+                .OrderBy(p => p.LastName)
+                .ThenBy(p => p.FirstName)
+                .ToList();
+
+            ViewBag.IntlCollegiateBaseballConfRoster = _context.Players
+                .Where(p => p.CurrentTeam.CurrLeague.Name.Contains("International Collegiate Baseball Conference"))
+                .OrderBy(p => p.LastName)
+                .ThenBy(p => p.FirstName)
+                .ToList();
+
+            ViewBag.AmericanConfAmFootball_Lopez = _context.Players
+                .Where(p => p.CurrentTeam.CurrLeague.Name.Contains("American Conference of Amateur Football") && p.LastName.Contains("Lopez"))
+                .OrderBy(p => p.LastName)
+                .ThenBy(p => p.FirstName)
+                .ToList();
+
+            ViewBag.AllFootballRoster = _context.Players
+                .Where(p => p.CurrentTeam.CurrLeague.Sport == "Football")
+                .OrderBy(p => p.LastName)
+                .ThenBy(p => p.FirstName)
+                .ToList();
+
+            ViewBag.TeamsWithASophia = _context.Players
+                .Where(p => p.FirstName == "Sophia" || p.LastName == "Sophia")
+                .Select(p => p.CurrentTeam)
+                .Distinct()
+                .OrderBy(team => team.TeamName)
+                .ThenBy(team => team.Location)
+                .ToList();
+            
+            #region [This helps for a comparison using Razor]
+            ViewBag.SophiaRoster = _context.Players
+                .Where(p => p.FirstName == "Sophia" || p.LastName == "Sophia")
+                .OrderBy(p => p.LastName)
+                .ThenBy(p => p.FirstName)
+                .ToList();
+            #endregion
+
+            ViewBag.LeaguesWithSophia = _context.Players
+                .Where(p => p.FirstName == "Sophia" || p.LastName == "Sophia")
+                .Select(p => p.CurrentTeam.CurrLeague)
+                .Distinct()
+                .OrderBy(l => l.Sport)
+                .ThenBy(l => l.Name)
+                .ToList();
+
+            ViewBag.FloresRoster_NonRoughRider = _context.Players
+                .Where(p => p.LastName.Contains("Flores") && p.CurrentTeam.Location + p.CurrentTeam.TeamName != "Washington Roughriders")
+                .Include(p => p.CurrentTeam)
+                .OrderBy(p => p.LastName)
+                .ThenBy(p => p.FirstName)
+                .ToList();
+
             return View();
         }
 
         [HttpGet("level_3")]
         public IActionResult Level3()
         {
+            ViewBag.SamuelEvansTeams = _context.Teams
+                .Include(team => team.AllPlayers)
+                    .ThenInclude(playerteam => playerteam.PlayerOnTeam)
+                .Where(team => team.AllPlayers.Any(player => player.PlayerOnTeam.FirstName == "Samuel" && player.PlayerOnTeam.LastName == "Evans"))
+                .Include(team => team.CurrLeague)
+                .ToList();
+
+            ViewBag.ManitobaTigerPlayers = _context.Players
+                .Include(player => player.AllTeams)
+                    .ThenInclude(playerteam => playerteam.PlayerOnTeam)
+                .Where(player => player.AllTeams.Any(player => player.TeamOfPlayer.Location == "Manitoba" && player.TeamOfPlayer.TeamName == "Tiger-Cats"))
+                .OrderBy(player => player.LastName)
+                .ThenBy(player => player.FirstName)
+                .ToList();
+
+            ViewBag.FormerWVikings = _context.Players
+                .Include(p => p.AllTeams)
+                    .ThenInclude(pt => pt.PlayerOnTeam)
+                .Include(p => p.CurrentTeam)
+                .Where(p => p.AllTeams.Any(p => p.TeamOfPlayer.Location == "Wichita" && p.TeamOfPlayer.TeamName == "Vikings") && p.CurrentTeam.TeamName != "Vikings")
+                .OrderBy(p => p.LastName)
+                .ThenBy(p => p.FirstName)
+                .ToList();
+
+            ViewBag.JacobGreyTeamsB4Colts = _context.Teams
+                .Include(team => team.AllPlayers)
+                    .ThenInclude(pt => pt.PlayerOnTeam)
+                .Include(team => team.CurrentPlayers)
+                .Where(team => team.AllPlayers.Any(pt => pt.PlayerOnTeam.FirstName == "Jacob" && pt.PlayerOnTeam.LastName == "Gray") && (team.Location + team.TeamName != "Oregon Colts"))
+                .Include(team => team.CurrLeague)
+                .OrderBy(team => team.Location)
+                .ThenBy(team => team.TeamName)
+                .ToList();
+
+            ViewBag.JoshuasInAFABaseball = _context.Players
+                .Where(p => p.FirstName == "Joshua")
+                .Include(p => p.AllTeams)
+                    .ThenInclude(pt => pt.TeamOfPlayer)
+                        .ThenInclude(team => team.CurrLeague)
+                .Where(p => p.AllTeams.Any(team => team.TeamOfPlayer.CurrLeague.Name == "Atlantic Federation of Amateur Baseball Players"))
+                .ToList();
+            
+            #region [Proof that there were no Joshua players in the Atlantic Federation of Amateur Baseball]
+            
+            var AFABaseballPlayers = _context.Players
+                .Where(p => p.FirstName == "Joshua")
+                .Include(p => p.AllTeams)
+                    .ThenInclude(pt => pt.TeamOfPlayer)
+                        .ThenInclude(team => team.CurrLeague)
+                .ToList();
+            
+            foreach (var p in AFABaseballPlayers)
+            {
+                Console.WriteLine($"{p.FirstName} {p.LastName}:");
+                foreach (var t in p.AllTeams)
+                {
+                    Console.WriteLine($"\t{t.TeamOfPlayer.Location} {t.TeamOfPlayer.TeamName}, {t.TeamOfPlayer.CurrLeague.Name}");
+                }
+            }
+            #endregion
+
+            ViewBag.AnyTeamWith12Plus = _context.Teams
+                .Include(t => t.AllPlayers)
+                .Where(t => t.AllPlayers.Count > 11)
+                .OrderBy(t => t.Location)
+                .ThenBy(t => t.TeamName)
+                .ToList();
+
+            ViewBag.PlayersSortedNumTeamsPlayedFor = _context.Players
+                .Include(p => p.AllTeams)
+                .OrderByDescending(p => p.AllTeams.Count)
+                .ThenBy(p => p.LastName)
+                .ThenBy(p => p.FirstName)
+                .ToList();
+
             return View();
         }
-
     }
 }
